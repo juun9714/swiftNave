497e6e7bcc2fcf6417235243c7d454f259121026
권준희 <june@gwonjunhuiui-MacBookAir.local>
2022-07-21 00:30:58 +0900

    nav view controller

---
+++ NavProject/TwoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TwoViewController.swift
+//  NavProject
+//
+//  Created by 권준희 on 2022/07/20.
+//
+
+import UIKit
+
+class TwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
